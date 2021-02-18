5a107bfdad3e78807254d5868020a3c2a853de3f
kr0kl <kr0kl@yandex.ru>
2021-02-18 15:40:48 +0300

    1.8. Состояние объекта [#451102]

---
+++ src/main/java/ru/job4j/tracker/ru/job4j/oop/Battery.java
@@ -0,0 +1,9 @@
+package ru.job4j.tracker.ru.job4j.oop;
+
+public class Battery {
+    private int load;
+    public void exchange(Battery another){
+        another.load = another.load + this.load;
+        this.load = 0;
+    }
+}
