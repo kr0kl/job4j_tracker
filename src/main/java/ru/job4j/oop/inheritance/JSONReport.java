63ef7b7f5365ee190ebfec28643f12eb9e11ebf3
kr0kl <kr0kl@yandex.ru>
2021-02-25 12:53:50 +0300

    4. Переопределение [#451109]

--- src/main/java/ru/job4j/oop/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/oop/inheritance/JSONReport.java
@@ -3,6 +3,6 @@ package ru.job4j.oop.inheritance;
 public class JSONReport extends TextReport{
     @Override
     public String generate(String name, String body){
-        return "{"+ System.lineSeparator() + "\"name\"" + ":\"name\"" + "," + System.lineSeparator() + "\"body\"" + ":\"body\"" + System.lineSeparator() + "}";
+        return "{"+ System.lineSeparator() + "\"name\"" + ":" + name + "," + System.lineSeparator() + "\"body\"" + ":" + body + System.lineSeparator() + "}";
     }
 }
