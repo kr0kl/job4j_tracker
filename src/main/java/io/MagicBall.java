2a8f7716f87771b31ff529115abf95d9b7c79a05
kr0kl <kr0kl@yandex.ru>
2021-03-14 22:23:52 +0300

    1. Чтение из консоли. Класс Scanner. [#451045]

---
+++ src/main/java/io/MagicBall.java
@@ -0,0 +1,21 @@
+package io;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class MagicBall {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Спрашивай.");
+        String name = input.nextLine();
+        int answer = new Random().nextInt(3);
+        if (answer == 0) {
+            System.out.println("Да");
+        } else if (answer == 1) {
+            System.out.println("Нет");
+        } else {
+            System.out.println("Может быть");
+        }
+
+    }
+}
